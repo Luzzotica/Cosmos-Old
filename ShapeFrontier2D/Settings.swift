779e88d92b1518950a2b14d65a10d29ea03e8648
Andrew Pond <apond308@gmail.com>
2018-05-03 22:13:17 -0600

    Added touch delay to touch controller

---
+++ ShapeFrontier2D/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  ShapeFrontier2D
+//
+//  Created by Andrew Pond on 5/3/18.
+//  Copyright © 2018 Sterling Long. All rights reserved.
+//
+
+import Foundation
+
+struct GameSettings {
+    
+}
