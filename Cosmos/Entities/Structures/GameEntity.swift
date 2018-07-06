3e51507be3249ceec00f2af5e5ae66e438d55f3b
Sterling Long <luzzotica@gmail.com>
2018-07-05 21:51:51 -0600

    Moved sprite setup to GameEntity

--- Cosmos/Entities/Structures/GameEntity.swift
+++ Cosmos/Entities/Structures/GameEntity.swift
@@ -21,3 +21,20 @@ protocol GameplayEntity
     func didDied()
     
 }
+
+extension GameplayEntity
+{
+    static func setupSprite(texture: SKTexture, size: CGSize) -> SpriteComponent
+    {
+        // Add a sprite
+        let spriteComponent = SpriteComponent(texture: texture, size: size)
+        
+        spriteComponent.node.physicsBody?.categoryBitMask = CollisionType.Structure
+        spriteComponent.node.physicsBody?.contactTestBitMask = CollisionType.Structure
+        spriteComponent.node.physicsBody?.collisionBitMask = CollisionType.Nothing
+        
+        spriteComponent.node.zPosition = Layer.Player
+        
+        return spriteComponent
+    }
+}
--- Cosmos/Entities/Structures/Structure2.0.swift
+++ Cosmos/Entities/Structures/Structure2.0.swift
@@ -43,20 +43,5 @@ protocol Structure2: GameplayEntity
     
 }
 
-extension Structure2
-{
-    static func setupSprite(texture: SKTexture, size: CGSize) -> SpriteComponent
-    {
-        // Add a sprite
-        let spriteComponent = SpriteComponent(texture: texture, size: size)
-        
-        spriteComponent.node.physicsBody?.categoryBitMask = CollisionType.Structure
-        spriteComponent.node.physicsBody?.contactTestBitMask = CollisionType.Structure
-        spriteComponent.node.physicsBody?.collisionBitMask = CollisionType.Nothing
-        
-        spriteComponent.node.zPosition = Layer.Player
-        
-        return spriteComponent
-    }
-}
+
 
